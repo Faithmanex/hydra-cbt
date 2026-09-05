7b306aaa67d2c7d63dd83b0f9c4e5b4aaaeddc7b
Faithman <fmanekponne@gmail.com>
2026-09-05 09:27:36 +0100

    Redesign client UI: sleek glass design system, new typography, refined camera and answer cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -169,7 +169,7 @@ export default function App() {
       <header className="topbar">
         <div className="brand">
           <div className="brand-mark">
-            <svg viewBox="0 0 24 24" width="20" height="20" fill="#04121c">
+            <svg viewBox="0 0 24 24" width="20" height="20" fill="#ffffff">
               <path d="M9 3L7.2 5H5a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2.2L15 3H9zm3 14a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-8a3 3 0 1 0 0 6 3 3 0 0 0 0-6z" />
             </svg>
           </div>
